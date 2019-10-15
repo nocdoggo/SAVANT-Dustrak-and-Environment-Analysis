ba918ff822955d161374fdaa9b08e01f80ddb7f0
nocdoggo <45600261+nocdoggo@users.noreply.github.com>
2019-10-15 12:02:53 -0500

    Updated

--- analysis.m
+++ analysis.m
@@ -719,7 +719,7 @@ if isWindExist == 'Y'
     
     
     % Bulk Richardson Number
-    for iteration = 1:10     % As for the theta_v, we are using the lower surface temp
+    for iteration = 1:11     % As for the theta_v, we are using the lower surface temp
         if iteration == 1
             upper_height = 4.5;
             lower_height = 1.5;
@@ -1197,7 +1197,7 @@ if isWindExist == 'Y'
             table_name_uconv_bulk = strcat(targetDate, '_calculation_init_3.0-4.5.csv');
             writetable(tempT_uconv_bulk, table_name_uconv_bulk);
         elseif iteration == 7
-                        % Init Tower
+            % Init Tower
             upper_height = 6.0;
             lower_height = 4.5;
 %             delta_theta_v = temperatureTable{:, 19} - temperatureTable{:, 18};
@@ -1265,8 +1265,8 @@ if isWindExist == 'Y'
             table_name_uconv_bulk = strcat(targetDate, '_calculation_init_4.5-6.0.csv');
             writetable(tempT_uconv_bulk, table_name_uconv_bulk);
             
-        elseif iteration == 7
-                        % Init Tower
+        elseif iteration == 8
+             % Init Tower
             upper_height = 10.0;
             lower_height = 6.0;
 %             delta_theta_v = temperatureTable{:, 19} - temperatureTable{:, 18};
@@ -1334,7 +1334,7 @@ if isWindExist == 'Y'
             table_name_uconv_bulk = strcat(targetDate, '_calculation_init_6.0-10.0.csv');
             writetable(tempT_uconv_bulk, table_name_uconv_bulk);
             
-        elseif iteration == 8
+        elseif iteration == 9
             % Rel Tower
             upper_height = 15.0;
             lower_height = 10.0;
@@ -1404,7 +1404,7 @@ if isWindExist == 'Y'
             writetable(tempT_uconv_bulk, table_name_uconv_bulk);
 
             
-        elseif iteration == 9
+        elseif iteration == 10
             
             % Rel Tower
             upper_height = 20.0;
@@ -1474,9 +1474,75 @@ if isWindExist == 'Y'
             table_name_uconv_bulk = strcat(targetDate, '_calculation_rel_15.0-20.0.csv');
             writetable(tempT_uconv_bulk, table_name_uconv_bulk);
 
-        elseif iteration == 10
+        elseif iteration == 11
             
+            % Rel Tower
+            upper_height = 6.0;
+            lower_height = 4.5;
+            %             delta_theta_v = temperatureTable{:, 19} - temperatureTable{:, 18};
+            delta_theta_v = windTable{:, 162} - windTable{:, 161};        % tc_high_tower - tc_low_tower
+            delta_z = upper_height - lower_height;
+            %             theta_v = temperatureTable{:, 18};
+            theta_v_low = windTable{:, 161} + 273;           % tc_low_tower + 273
+            theta_v_high = windTable{:, 162} + 273;          % tc_high_tower + 273
+            delta_u = windTable{:, 117} - windTable{:, 116};    % u_high_tower - u_low_tower
+            delta_v = windTable{:, 132} - windTable{:, 131};  % v_high_tower - v_low_height
+            u_star_low = (((windTable{:, 191}).*(windTable{:, 191})) + ((windTable{:, 206}).*(windTable{:, 206}))).^(0.25);     % u_w__low_tower * u_w__low_tower   + v_w__low_tower * v_w__low_tower
+            u_star_high = (((windTable{:, 192}).*(windTable{:, 192})) + ((windTable{:, 207}).*(windTable{:, 207}))).^(0.25);    % u_w__high_tower * u_w__high_tower + v_w__high_tower * v_w__high_tower
+            w_theta_v_low = windTable{:, 176};               % w_tc__low_tower
+            w_theta_v_high = windTable{:, 177};              % w_tc__high_tower
             
+            % Relialization for R_Bulk
+            R_Bulk_up = [];
+            R_Bulk_down = [];
+            R_Bulk_Rel_4_5_6 = [];
+            L_up_low = [];
+            L_down_low = [];
+            L_Rel_4_5 = [];
+            L_up_high = [];
+            L_down_high = [];
+            L_Rel_6 = [];
+            Term_3_Rel_4_5 = [];
+            Term_3_Rel_6 = [];
+            
+            % Try to calculate the R_Bulk
+            for idx = 1:length(CDT_Time)
+                try
+                    R_Bulk_up(idx, 1) = g .* delta_theta_v(idx, 1) * delta_z;
+                    R_Bulk_down(idx, 1) = theta_v_low(idx, 1) .* ((delta_u(idx, 1).*delta_u(idx, 1)) + (delta_v(idx, 1).*delta_v(idx, 1)));
+                    R_Bulk_Rel_4_5_6(idx, 1) = R_Bulk_up(idx, 1) ./ R_Bulk_down(idx, 1);
+                catch
+                    errmsg('red','bulk Richardson number is not calculatable @: \n');
+                    errmsg('blue', '      %s\n',CDT_Time(idx, 1));
+                end
+                
+                % Try to calculate the lower surface Obukhov length
+                try
+                    L_up_low(idx, 1) = -theta_v_low(idx, 1) .* ((u_star_low(idx, 1)).^3);
+                    L_down_low(idx, 1) = k * g .* w_theta_v_low(idx, 1);
+                    L_Rel_4_5(idx, 1) = L_up_low(idx, 1) ./ L_down_low(idx, 1);
+                    Term_3_Rel_4_5(idx, 1) = lower_height ./ L_Rel_4_5(idx, 1);
+                catch
+                    errmsg('red','Obukhov length for lower surface is not calculatable @: \n');
+                    errmsg('blue', '      %s\n',CDT_Time(idx, 1));
+                end
+                
+                % Try to calculate the lower surface Obukhov length
+                try
+                    L_up_high(idx, 1) = -theta_v_high(idx, 1) .* ((u_star_high(idx, 1)).^3);
+                    L_down_high(idx, 1) = k * g .* w_theta_v_high(idx, 1);
+                    L_Rel_6(idx, 1) = L_up_high(idx, 1) ./ L_down_high(idx, 1);
+                    Term_3_Rel_6(idx, 1) = upper_height ./ L_Rel_6(idx, 1);
+                catch
+                    errmsg('red','Obukhov length for higher surface is not calculatable @: \n');
+                    errmsg('blue', '      %s\n',CDT_Time(idx, 1));
+                end
+            end
+            
+            % Form a csv dump
+            tempT_uconv_bulk = table(CDT_Time, bar_fence, R_Bulk_Rel_4_5_6, L_Rel_4_5, L_Rel_6, Term_3_Rel_4_5, Term_3_Rel_6);
+            table_name_uconv_bulk = strcat(targetDate, '_calculation_rel_4.5-6.0.csv');
+            writetable(tempT_uconv_bulk, table_name_uconv_bulk);
         end
         
     end
